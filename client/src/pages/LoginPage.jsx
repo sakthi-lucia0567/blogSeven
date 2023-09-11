afa93d9a9b9e99d275f79000ceb441b446950451
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2023-09-11 23:49:43 +0530

    feat: notistack and login complete successfully

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,34 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { useSnackbar } from "notistack";
 
 const LoginPage = () => {
+  const [loading, setLoading] = useState("");
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log("data coming from form", { email, password });
+
+    const data = {
+      email,
+      password,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:8000/auth/login", data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Login Successfully", {
+          variant: "success",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        enqueueSnackbar("User Not Exist", {
+          variant: "error",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
+        console.log(err);
+      });
+  };
+
   return (
-    <div class="m-auto mx-auto w-full max-w-sm rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
-      <form class="mt-6">
+    <div className="m-auto mx-auto w-full max-w-sm rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
+      {loading ? <Spinner /> : ""}
+      <form className="mt-6" onSubmit={handleSubmit}>
         <div>
           <label
-            for="username"
-            class="block text-sm text-gray-800 dark:text-gray-200"
+            htmlFor="email"
+            className="block text-sm text-gray-800 dark:text-gray-200"
           >
-            Username
+            Email
           </label>
           <input
-            type="text"
-            class="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
+            type="email"
+            name="email"
+            autoComplete="off"
+            className="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
           />
         </div>
 
-        <div class="mt-4">
-          <div class="flex items-center justify-between">
+        <div className="mt-4">
+          <div className="flex items-center justify-between">
             <label
-              for="password"
-              class="block text-sm text-gray-800 dark:text-gray-200"
+              htmlFor="password"
+              className="block text-sm text-gray-800 dark:text-gray-200"
             >
               Password
             </label>
             <a
               href="#"
-              class="text-xs text-gray-600 hover:underline dark:text-gray-400"
+              className="text-xs text-gray-600 hover:underline dark:text-gray-400"
             >
               Forget Password?
             </a>
@@ -36,23 +85,27 @@ const LoginPage = () => {
 
           <input
             type="password"
-            class="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
+            name="password"
+            className="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
           />
         </div>
 
-        <div class="mt-6">
-          <button class="w-full transform rounded-lg bg-gray-800 px-6 py-2.5 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+        <div className="mt-6">
+          <button
+            type="submit"
+            className="w-full transform rounded-lg bg-gray-800 px-6 py-2.5 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+          >
             Sign In
           </button>
         </div>
       </form>
 
-      <p class="mt-8 text-center text-xs font-light text-gray-400">
+      <p className="mt-8 text-center text-xs font-light text-gray-400">
         {" "}
         Don't have an account?{" "}
         <Link
           to="/signup"
-          class="font-medium text-gray-700 hover:underline dark:text-gray-200"
+          className="font-medium text-gray-700 hover:underline dark:text-gray-200"
         >
           Create One
         </Link>
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,28 +1,75 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { useSnackbar } from "notistack";
 
 const SignupPage = () => {
+  const [loading, setLoading] = useState("");
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    const confirmPassword = form.get("confirmPassword");
+    console.log("data coming from form", { email, password });
+
+    const data = {
+      email,
+      password,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:8000/auth/register", data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Signup Successfully", {
+          variant: "success",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
+        navigate("/login");
+      })
+      .catch((err) => {
+        setLoading(false);
+        enqueueSnackbar("User Not Exist", {
+          variant: "error",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
+        console.log(err);
+      });
+  };
+
   return (
-    <div class="m-auto mx-auto w-full max-w-sm rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
-      <form class="mt-6">
+    <div className="m-auto mx-auto w-full max-w-sm rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
+      <form className="mt-6" onSubmit={handleSubmit}>
         <div>
           <label
-            for="username"
-            class="block text-sm text-gray-800 dark:text-gray-200"
+            htmlFor="Email"
+            className="block text-sm text-gray-800 dark:text-gray-200"
           >
-            Username
+            Email
           </label>
           <input
-            type="text"
-            class="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
+            type="email"
+            className="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
           />
         </div>
 
-        <div class="mt-4">
-          <div class="flex items-center justify-between">
+        <div className="mt-4">
+          <div className="flex items-center justify-between">
             <label
-              for="password"
-              class="block text-sm text-gray-800 dark:text-gray-200"
+              htmlFor="password"
+              className="block text-sm text-gray-800 dark:text-gray-200"
             >
               Password
             </label>
@@ -30,37 +77,39 @@ const SignupPage = () => {
 
           <input
             type="password"
-            class="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
+            name="password"
+            className="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
           />
         </div>
-        <div class="mt-4">
-          <div class="flex items-center justify-between">
+        <div className="mt-4">
+          <div className="flex items-center justify-between">
             <label
-              for="confirmPassword"
-              class="block text-sm text-gray-800 dark:text-gray-200"
+              htmlFor="confirmPassword"
+              className="block text-sm text-gray-800 dark:text-gray-200"
             >
               Confirm Password
             </label>
           </div>
 
           <input
-            type="password"
-            class="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
+            type="confirmPassword"
+            name="confirmPassword"
+            className="mt-2 block w-full rounded-lg border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300"
           />
         </div>
 
-        <div class="mt-6">
-          <button class="w-full transform rounded-lg bg-gray-800 px-6 py-2.5 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+        <div className="mt-6">
+          <button className="w-full transform rounded-lg bg-gray-800 px-6 py-2.5 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
             Sign Up
           </button>
         </div>
       </form>
 
-      <p class="mt-8 text-center text-xs font-light text-gray-400">
+      <p className="mt-8 text-center text-xs font-light text-gray-400">
         Already have an account?
         <Link
           to="/login"
-          class="font-medium text-gray-700 hover:underline dark:text-gray-200"
+          className="font-medium text-gray-700 hover:underline dark:text-gray-200"
         >
           Create One
         </Link>
